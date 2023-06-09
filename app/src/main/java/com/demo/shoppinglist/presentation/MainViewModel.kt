c315accc31c083bdb82f3d66cdb22c8b5f3309be
dgmedvedev <dg-medvedev@mail.ru>
2023-06-09 12:14:32 +0300

    ViewModel test

--- app/src/main/java/com/demo/shoppinglist/presentation/MainViewModel.kt
+++ app/src/main/java/com/demo/shoppinglist/presentation/MainViewModel.kt
@@ -1,10 +1,9 @@
 package com.demo.shoppinglist.presentation
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.demo.shoppinglist.data.ShopListRepositoryImpl
-import com.demo.shoppinglist.domain.DeleteShopItemUseCase
-import com.demo.shoppinglist.domain.EditShopItemUseCase
-import com.demo.shoppinglist.domain.GetShopListUseCase
+import com.demo.shoppinglist.domain.*
 
 class MainViewModel() : ViewModel() {
 
@@ -14,4 +13,22 @@ class MainViewModel() : ViewModel() {
     private val getShopListUseCase = GetShopListUseCase(repository)
     private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
     private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList() {
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+    }
+
+    fun deleteShopItem(shopItem: ShopItem) {
+        deleteShopItemUseCase.deleteShopItem(shopItem)
+        getShopList()
+    }
+
+    fun changeEnableState(shopItem: ShopItem) {
+        val newItem = shopItem.copy(enable = !shopItem.enable)
+        editShopItemUseCase.editShopItem(newItem)
+        getShopList()
+    }
 }
