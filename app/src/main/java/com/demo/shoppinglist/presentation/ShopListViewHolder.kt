976170a1045e643e66d6fe53571fbed79e316d50
dgmedvedev <dg-medvedev@mail.ru>
2023-07-11 21:08:34 +0300

    DataBinding inside RecyclerView complete

--- app/src/main/java/com/demo/shoppinglist/presentation/ShopListViewHolder.kt
+++ app/src/main/java/com/demo/shoppinglist/presentation/ShopListViewHolder.kt
@@ -1,11 +1,7 @@
 package com.demo.shoppinglist.presentation
 
-import android.view.View
-import android.widget.TextView
+import androidx.databinding.ViewDataBinding
 import androidx.recyclerview.widget.RecyclerView
-import com.demo.shoppinglist.R
 
-class ShopListViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
-    val tvName: TextView = view.findViewById(R.id.tv_name)
-    val tvCount: TextView = view.findViewById(R.id.tv_count)
-}
+class ShopListViewHolder(val binding: ViewDataBinding) :
+    RecyclerView.ViewHolder(binding.root)
