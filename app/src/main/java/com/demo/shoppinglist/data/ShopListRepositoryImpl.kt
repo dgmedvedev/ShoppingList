c37484c05230e17d14a2268716a934704b6d409d
dgmedvedev <dg-medvedev@mail.ru>
2023-07-21 21:04:07 +0300

    repository complete

--- app/src/main/java/com/demo/shoppinglist/data/ShopListRepositoryImpl.kt
+++ app/src/main/java/com/demo/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -2,6 +2,7 @@ package com.demo.shoppinglist.data
 
 import android.app.Application
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import com.demo.shoppinglist.domain.ShopItem
 import com.demo.shoppinglist.domain.ShopListRepository
 
@@ -15,17 +16,22 @@ class ShopListRepositoryImpl(application: Application) : ShopListRepository {
     }
 
     override fun deleteShopItem(shopItem: ShopItem) {
+        shopListDao.deleteShopItem(shopItem.id)
     }
 
     override fun editShopItem(shopItem: ShopItem) {
+        shopListDao.addShopItem(mapper.mapEntityToDbModel(shopItem))
     }
 
     override fun getShopItem(shopItemId: Int): ShopItem {
+        val dbModel = shopListDao.getShopItem(shopItemId)
+        return mapper.mapDbModelToEntity(dbModel)
     }
 
-    override fun getShopList(): LiveData<List<ShopItem>> {
-    }
-
-    private fun updateList() {
-    }
+    override fun getShopList(): LiveData<List<ShopItem>> =
+        MediatorLiveData<List<ShopItem>>().apply {
+            addSource(shopListDao.getShopList()) {
+                value = mapper.mapListDbModelToListEntity(it)
+            }
+        }
 }
